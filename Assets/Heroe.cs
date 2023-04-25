8dc4fccc5cdfceb3f1732dd8fb1a0c3b99c28810
y4m4x <112440204+y4m4x@users.noreply.github.com>
2023-04-25 20:44:53 -0300

    0.1

---
+++ Assets/Heroe.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroe : Personaje
+{
+    public void specialAttack() { }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
